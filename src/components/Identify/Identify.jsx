c605f5ed1b982e7cba3e06c8e871ab06d3edf67f
Akshay Kumar Sharma <sharmaakshaykumar7807@gmail.com>
2023-06-20 17:21:34 +0000

    adding identify api

--- src/components/Identify/Identify.jsx
+++ src/components/Identify/Identify.jsx
@@ -1,13 +1,37 @@
-import { ActionIcon, Container, Title } from '@mantine/core'
-import React from 'react'
+import { ActionIcon, Button, Container, FileInput, Group, Select, Stack, Title } from '@mantine/core'
+import { useForm } from '@mantine/form'
+import React, { useState } from 'react'
 
 export default function Identify() {
+    const [image, setImage] = useState();
+    const form = useForm({
+        initialValues: {
+            organs: "auto"
+        }
+    })
+    let identify = (values) => {
+        fetch("https://my-api.plantnet.org/v2/identify/all" + "?api-key=2b10G793uJs1Eceq9ckepgyL5O",
+            {
+                method: "POST",
+                data: {
+                    "project": "all",
+                    "images": [image],
+                }
+            })
+    }
     return (
         <Container>
             <Title order={2}>Identify</Title>
-            <ActionIcon size='xl' radius="xl" variant="light" color="orange">
+            {/* <ActionIcon size='xl' radius="xl" variant="light" color="orange">
                 <i className='bi bi-camera'></i>
-            </ActionIcon>
+            </ActionIcon> */}
+            <form onSubmit={form.onSubmit((values) => identify(values))}>
+                <Stack align="center">
+                    <FileInput label="Select Image" placeholder="Select Image" icon={<i className="bi bi-camera" />} onChange={setImage} value={image} />
+                    <Select {...form.getInputProps("organs")} data={["auto", "leaf", "branch", "flower", "fruit", "bark"]}></Select>
+                    <Button type="submit">Submit</Button>
+                </Stack>
+            </form>
         </Container>
     )
 }
