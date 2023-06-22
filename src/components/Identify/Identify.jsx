2cfbbacd185e24b6d9235efa1d5ec7f036ba4195
Akshay Kumar Sharma <sharmaakshaykumar7807@gmail.com>
2023-06-22 10:43:08 +0000

    making request to identify plants

--- src/components/Identify/Identify.jsx
+++ src/components/Identify/Identify.jsx
@@ -1,23 +1,34 @@
 import { ActionIcon, Button, Container, FileInput, Group, Select, Stack, Title } from '@mantine/core'
 import { useForm } from '@mantine/form'
 import React, { useState } from 'react'
+import Info from './Info';
 
 export default function Identify() {
+    let [data, setData] = useState();
     const [image, setImage] = useState();
     const form = useForm({
         initialValues: {
             organs: "auto"
         }
     })
+    // data.append("images", [image])
     // console.log(image)
     let identify = (values) => {
+        let formData = new FormData();
+        formData.append("organs", values.organs)
+        formData.append("images", image)
         fetch("https://my-api.plantnet.org/v2/identify/all?include-related-images=false&no-reject=false&lang=en&api-key=2b10G793uJs1Eceq9ckepgyL5O",
             {
                 method: "POST",
-                data: {
-                    "project": "all",
-                    "images": [image],
-                }
+                body: formData
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+                setData(data)
+            })
+            .catch((err) => {
+                console.log("error: ", err);
             })
     }
     return (
@@ -33,6 +44,9 @@ export default function Identify() {
                     <Button type="submit">Submit</Button>
                 </Stack>
             </form>
+            {data &&
+                <Info data={data} />
+            }
         </Container>
     )
 }
---
+++ src/components/Identify/Info.jsx
@@ -0,0 +1,16 @@
+import { Container, Paper } from '@mantine/core'
+import React from 'react'
+
+export default function Info({ data }) {
+    return (
+        <Container>
+            {/* <Title order={2}>Results</Title>
+            {data.results.map((result) => {
+
+            })}
+            <Paper>
+                <Title order={3}></Title>
+            </Paper> */}
+        </Container>
+    )
+}
