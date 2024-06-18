620f30a3ac912837c143021a564e278b5fd8786e
sharmaakshaykumar7807 <sharmaakshaykumar7807@gmail.com>
2024-06-18 16:31:04 +0000

    updated libraries except mantine

---
+++ backup.js
@@ -0,0 +1,20 @@
+styles={{
+    main: {
+      background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+    },
+  }}
+
+
+  // Layout
+  // aside={
+        //   <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
+        //     <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
+        //       <Text>Application sidebar</Text>
+        //     </Aside>
+        //   </MediaQuery>
+        // }
+        // footer={
+        //   <Footer height={60} p="md">
+        //     Application footer
+        //   </Footer>
+        // }
