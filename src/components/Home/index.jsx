2441fc8b95de444b205bc3a901718b8df97db5fd
sharmaakshaykumar7807 <sharmaakshaykumar7807@gmail.com>
2024-08-09 13:49:34 +0000

    hompage fixed for mobile

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -11,11 +11,11 @@ export default function Home() {
         Protect and preserve our forests with real-time insights and proactive care. The Saplings Protector system leverages advanced technology to monitor, analyze, and safeguard trees, ensuring a healthier environment for generations to come.
         </Text>
 
-        <Group>
-        <Button component={Link} variant="gradient" gradient={{ from: 'teal', to: 'lime', deg: 90 }} size="xl" radius="xl" className={classes.control} to="/my-trees">
+        <Group className={classes.control}>
+        <Button component={Link} variant="gradient" gradient={{ from: 'teal', to: 'lime', deg: 90 }} size="xl" radius="xl" to="/my-trees">
           Get started
         </Button>
-        <Button component={Link} variant='light' color="green" className={classes.control} to="/about" size="xl" radius="xl">
+        <Button component={Link} variant='light' color="green"  to="/about" size="xl" radius="xl">
           Learn More
         </Button>
         
