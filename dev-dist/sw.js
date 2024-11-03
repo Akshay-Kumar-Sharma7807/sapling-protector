25cd27094576e511a5e235f7b9f96be7c8ee50fc
Akshay Kumar Sharma <82942162+Akshay-Kumar-Sharma7807@users.noreply.github.com>
2024-11-03 02:01:34 +0000

    added auto close on accoutn

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-9637eeee'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8hidnui21ho"
+    "revision": "0.vj6gss1sej"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
